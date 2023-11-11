1bab024311b570fdf861b455265e9fac7fdea744
roger.delacruz.jr@gmail.com <roger.delacruz.jr@gmail.com>
2023-11-11 02:09:58 -0500

    Added SQL logic to api for querying update posts

--- api/updates.ts
+++ api/updates.ts
@@ -1,7 +1,13 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { sql } from '@vercel/postgres';
 
 async function getUpdates(request: VercelRequest, response: VercelResponse) {
-  return response.status(200).send(true);
+  try {
+    const result = await sql`SELECT post_id, title, content, image, post_date, post_by FROM posts`;
+    return response.status(200).json({ result });
+  } catch (error) {
+    return response.status(500).json({ error });
+  }
 }
 
 export default async function handler(request: VercelRequest, response: VercelResponse) {
--- types.d.ts
+++ types.d.ts
@@ -6,3 +6,21 @@ declare module '*.svg' {
   const imageUrl: string
   export default imageUrl
 }
+
+// Vite-specific
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  readonly POSTGRES_URL: string;
+  readonly POSTGRES_PRISMA_URL: string;
+  readonly POSTGRES_URL_NON_POOLING: string;
+  readonly POSTGRES_USER: string;
+  readonly POSTGRES_HOST: string;
+  readonly POSTGRES_PASSWORD: string;
+  readonly POSTGRES_DATABASE: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}

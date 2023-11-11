c6dffab5935ebc3845dea4344deecd0367fbe587
roger.delacruz.jr@gmail.com <roger.delacruz.jr@gmail.com>
2023-11-11 00:42:48 -0500

    Switched with js file

--- api/ssr.js
+++ api/ssr.js
@@ -1,11 +1,12 @@
 import { renderPage } from 'vike/server';
-import { VercelRequest, VercelResponse } from '@vercel/node';
+
+// We use JSDoc instead of TypeScript because Vercel seems buggy with /api/**/*.ts files
 
 /**
  * @param {import('@vercel/node').VercelRequest} req
  * @param {import('@vercel/node').VercelResponse} res
  */
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(req, res) {
   const { url } = req;
   console.log('Request to url:', url);
   if (url === undefined) throw new Error('req.url is undefined');

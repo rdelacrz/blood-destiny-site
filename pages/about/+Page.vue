9f4b52e4cebaa8f1a22bb7123b3e1817a32c5aab
roger.delacruz.jr@gmail.com <roger.delacruz.jr@gmail.com>
2023-12-02 20:25:43 -0500

    Updated about section

--- pages/about/+Page.vue
+++ pages/about/+Page.vue
@@ -5,15 +5,18 @@
     </div>
     <div class='story-content-wrapper'>
       <p class='general-text'>
-        Blood Destiny is a visual novel that takes place in a fictional version of the world, during the year 2245. About 200 years
-        have passed since the Third World war that devastated the whole planet, which is now divided into four enormous supernations.
-        The story revolves around a young mercenary named Jack Smith, who is part of an organization named Olympia which accepts contracts
-        from the four nations to deal with supernatural threats.
+        Blood Destiny is a visual novel that takes place in a fictional version of the world, during the year 2245. About 200 years have 
+        passed since the Third World war that devastated the whole planet, which is now divided into four enormous supernations. The story 
+        revolves around a young mercenary named Jack Smith, who is part of an organization named Olympia which accepts contracts from the four 
+        nations to deal with supernatural threats. Jack is a proud and confident fighter who aspires to become the strongest in the world. Over 
+        the course of the story, he will be drawn into various conflicts involving legendary creatures, ancient civilizations lost to history, and 
+        unsolved mysterious from his own past.
       </p>
       <p class='general-text'>
-        The player may choose different choices throughout the novel to influence the story's direction. Once every regular route has been
-        cleared, the final route will be unlocked which will lead to the True Ending of the story. There will also be turn-based RPG battles
-        where the player will have to fight various powerful enemies.
+        There are several heroines in the story that will serve as potential love interests to Jack. The player may select different choices 
+        throughout the novel to influence the story's direction, and potentially which heroine he ends up with. Once every regular route has 
+        been cleared, the final route will be unlocked which will lead to the True Ending of the story. There will also be turn-based RPG battles 
+        where the player will have to fight various enemies.
       </p>
     </div>
     <div class='screenshots-row-container'>

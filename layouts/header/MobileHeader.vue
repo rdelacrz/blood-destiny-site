bda2993025d25cc2b8fb3170197930b84309a191
roger.delacruz.jr@gmail.com <roger.delacruz.jr@gmail.com>
2024-02-11 01:16:01 -0500

    Fixed header

--- layouts/header/MobileHeader.vue
+++ layouts/header/MobileHeader.vue
@@ -8,7 +8,7 @@
     <Transition>
       <ul v-show="menuActive" class="header-links-wrapper">
         <li><AppLink to="/" @click="toggleMenu">Home</AppLink></li>
-        <li><AppLink to="/about" :backgroundSrc="aboutBackground" @click="toggleMenu">About</AppLink></li>
+        <li><AppLink to="/about" @click="toggleMenu">About</AppLink></li>
         <li><AppLink to="/characters" @click="toggleMenu">Characters</AppLink></li>
         <li><AppLink to="/soundtrack" @click="toggleMenu">Soundtrack</AppLink></li>
         <li><AppLink to="/updates" @click="toggleMenu">Updates</AppLink></li>
@@ -23,12 +23,6 @@ import { defineAsyncComponent } from "vue";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
-import buttonAbout from "@/assets/images/backgrounds/buttons/button_about.png";
-import buttonCharacters from "@/assets/images/backgrounds/buttons/button_characters.png";
-import buttonContactUs from "@/assets/images/backgrounds/buttons/button_contact_us.png";
-import buttonSoundtrack from "@/assets/images/backgrounds/buttons/button_soundtrack.png";
-import buttonUpdates from "@/assets/images/backgrounds/buttons/button_updates.png";
-
 export default {
   name: "page-header",
   components: {
@@ -38,11 +32,6 @@ export default {
   data() {
     return {
       menuActive: false,
-      aboutBackground: buttonAbout,
-      charactersBackground: buttonCharacters,
-      contactUsBackground: buttonContactUs,
-      soundtrackBackground: buttonSoundtrack,
-      updatesBackground: buttonUpdates,
       icons: {
         bars: faBars,
       },

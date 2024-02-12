5e02459fc66679f6e14e9698e17f41a054c32c59
roger.delacruz.jr@gmail.com <roger.delacruz.jr@gmail.com>
2024-02-11 21:17:44 -0500

    Gates console logs behind env check

--- renderer/onPageTransitionHooks.ts
+++ renderer/onPageTransitionHooks.ts
@@ -4,13 +4,19 @@ export { onHydrationEnd, onPageTransitionStart, onPageTransitionEnd }
 import type { OnHydrationEndAsync, OnPageTransitionEndAsync, OnPageTransitionStartAsync } from "vike/types";
 
 const onHydrationEnd: OnHydrationEndAsync = async (): ReturnType<OnHydrationEndAsync> => {
-  console.log("Hydration finished; page is now interactive.");
+  if (process.env.NODE_ENV !== "production") {
+    console.log("Hydration finished; page is now interactive.");
+  }
 }
 const onPageTransitionStart: OnPageTransitionStartAsync = async (): ReturnType<OnPageTransitionStartAsync> => {
-  console.log("Page transition start");
+  if (process.env.NODE_ENV !== "production") {
+    console.log("Page transition start");
+  }
   document.querySelector(".page-content")!.classList.add("page-transition");
 }
 const onPageTransitionEnd: OnPageTransitionEndAsync = async (): ReturnType<OnPageTransitionEndAsync> => {
-  console.log("Page transition end");
+  if (process.env.NODE_ENV !== "production") {
+    console.log("Page transition end");
+  }
   document.querySelector(".page-content")!.classList.remove("page-transition");
 }

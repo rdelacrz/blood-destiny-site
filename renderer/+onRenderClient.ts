63cd5d221d3b75a7002c53e468b9bf9eef71d0b5
roger.delacruz.jr@gmail.com <roger.delacruz.jr@gmail.com>
2024-02-22 01:58:25 -0500

    Makes changePage async

--- renderer/+onRenderClient.ts
+++ renderer/+onRenderClient.ts
@@ -10,15 +10,19 @@ import "@/styles/global.scss";
 let createAppProps: ReturnType<typeof createApp>;
 const onRenderClient: OnRenderClientAsync = async (pageContext): ReturnType<OnRenderClientAsync> => {
   if (!createAppProps) {
+    // First rendering/hydration
+    
+    createAppProps = createApp(pageContext);
+    const { app } = createAppProps;
+    
     // Disable the automatic insertion of CSS into the head of the document (it is already inserted on server-side)
     config.autoAddCss = false;
 
-    createAppProps = createApp(pageContext);
-    const { app } = createAppProps;
     app.mount("#app");
   } else {
+    // Client-side navigation
     const { app } = createAppProps;
-    app.changePage(pageContext);
+    await app.changePage(pageContext);
   }
   document.title = getPageTitle(pageContext);
 }
